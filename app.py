be87d8ccccb63ac04a57ff4bfbe054147d3b67d1
minkyoJang <missrabbt@naver.com>
2021-09-21 00:59:21 +0900

    시작일,종료일 입력시 csv 파일 export 완료

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # app.py
-
-from flask import Flask, render_template, send_file
-from save import save_to_file
 import time
+import save
+import requests
 
+from flask import Flask, render_template, send_file, request
 
 # Flask 객체 인스턴스 생성
 app = Flask(__name__)
@@ -12,11 +12,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/export') # 접속하는 url
-def export():
-    save_to_file()
-    FN= str(time.strftime("%Y%m%d"))+".csv"
+
+@app.route('/post', methods=['POST']) # 접속하는 url
+def post():
+    value1 = request.form['userInput1']
+    value2 = request.form['userInput2']
+
+    save.save_to_file()
+    FN = str(value1)+"-"+str(value2) +".csv"
+    
+    with open(FN,mode = 'w') as f:
+        f.write("Hellow-World")
+    
     return send_file(FN,mimetype='application/x-csv',attachment_filename = FN, as_attachment=True)
+
+
 
 
 if __name__ == "__main__":
@@ -24,3 +34,4 @@ if __name__ == "__main__":
 
     # host 등을 직접 지정하고 싶다면
     # app.run(host = "127.0.0.1", port = "5000", debug = True)
+
